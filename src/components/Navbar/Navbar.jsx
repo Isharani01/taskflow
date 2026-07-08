f4e923b14a872dda6e59b8c93675c2eb2607582c
Isharani01 <ishakhizar2709@gmail.com>
2026-07-08 21:37:05 +0500

    designed premium responsive navigation bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { SquareCheckBig } from "lucide-react";
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <div className={styles.logoIcon}></div>
-          <span>TaskFlow</span>
+        <SquareCheckBig size={28} strokeWidth={2.5} />
+        <span>TaskFlow</span>
         </div>
-
         <ul className={styles.navLinks}>
           <li><NavLink to="/">Home</NavLink></li>
           <li><a href="#features">Features</a></li>
