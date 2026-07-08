c1b62d713423f584b028d7826b8a53e95f51c763
Isharani01 <ishakhizar2709@gmail.com>
2026-07-08 14:31:41 +0500

    configured application routing with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import {Routes,Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Dashboard from "./pages/Dashboard/Dashoard";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
 function App() {
   return (
-    <>
-      <h1>TaskFlow</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
   );
 }
 
