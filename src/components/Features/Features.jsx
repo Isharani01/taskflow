5530d4581d32b563c2dc4695855529b764d7af2b
Isharani01 <ishakhizar2709@gmail.com>
2026-07-09 18:30:00 +0500

    installed react icons and used in feature cards

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,4 +1,11 @@
 import styles from "./Features.module.css"
+import {
+  FaTasks,
+  FaChartLine,
+  FaUsers,
+  FaBell,
+  FaCloud
+} from "react-icons/fa";
 function Features(){
     return(
         <section className={styles.features}>
@@ -17,27 +24,27 @@ function Features(){
                 {/*track holds all cards*/}
                     <div className={styles.track}>
                     <div className={styles.card}>
-                        <div className={styles.icon}></div>
+                        <div className={styles.icon}><FaTasks /></div>
                         <h3>Task Management</h3>
                          <p>Organize and manage your daily tasks with ease.</p>
                     </div>
                     <div className={styles.card}>
-                    <div className={styles.icon}></div>
+                    <div className={styles.icon}> <FaChartLine /></div>
                      <h3>Analytics</h3>
                      <p> Track your progress with detailed insights.</p>
                     </div>
             <div className={styles.card}>
-              <div className={styles.icon}></div>
+              <div className={styles.icon}><FaUsers/></div>
               <h3>Team Collaboration</h3>
               <p> Work together with your team in real time.</p>
             </div>
             <div className={styles.card}>
-              <div className={styles.icon}></div>
+              <div className={styles.icon}><FaBell/></div>
               <h3>Smart Notifications</h3>
               <p> Never miss important deadlines or updates. </p>
             </div>
             <div className={styles.card}>
-              <div className={styles.icon}></div>
+              <div className={styles.icon}>< FaCloud/></div>
               <h3>Cloud Sync</h3>
               <p>  Access your tasks securely from any device.  </p>
             </div>   
