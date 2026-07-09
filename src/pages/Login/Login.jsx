2cee501ab2e50786e788ae1db383a448dae5761e
Isharani01 <ishakhizar2709@gmail.com>
2026-07-10 00:33:17 +0500

    basic structure of login page created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,35 @@
+import styles from "./Login.module.css"
+import { Link } from "react-router-dom";
+import {FaCheckCircle} from "react-icons/fa";
 function Login(){
     return(
-        <div>
-            <h1>Login page</h1>
+    <section className={styles.login}>
+        <div className={styles.container}>
+            <div className={styles.loginCard}>
+                <div className={styles.logo}> <FaCheckCircle />
+                TaskFlow</div>
+                <h1>Welcome Back</h1>
+                 <p> Sign in to continue managing your tasks.</p>
+                <form className={styles.form}>
+                    <div className={styles.inputGroup}>
+                    <label>Email Address</label>
+                    <input type="email" placeholder="Enter your email"/>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label>Password</label>
+                        <input type="password" placeholder="Enter your password" />
+                    </div>
+                    <button className={styles.loginBtn}>
+                     Sign In</button>
+                </form>
+                <div className={styles.registerLink}>
+                    <p>Don't have an account? <Link to ="/register">Register</Link>
+                    </p>
+                </div>
+
+            </div>
         </div>
+    </section>
 
     );    
 }
