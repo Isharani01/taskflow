694c5c5a9ed3c39e396be1a7e0b7ba4e82ca7032
Isharani01 <ishakhizar2709@gmail.com>
2026-07-12 01:55:48 +0500

    created NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
+import styles from "./NotFound.module.css";
+
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-    </div>
+    <section className={styles.notFound}>
+
+      <h1>404</h1>
+
+      <h2>Page Not Found</h2>
+
+      <p>
+        The page you are looking for doesn't exist.
+      </p>
+
+      <Link to="/" className={styles.btn}>
+        Go Back Home
+      </Link>
+
+    </section>
   );
 }
 
