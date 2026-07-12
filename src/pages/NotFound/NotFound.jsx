0abc914ff0b46556c7b8b5b1740d06b073b44ae2
Isharani01 <ishakhizar2709@gmail.com>
2026-07-12 14:39:25 +0500

    modified notfound page imported navbar and footer components

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,23 +1,29 @@
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 import styles from "./NotFound.module.css";
 
 function NotFound() {
   return (
-    <section className={styles.notFound}>
+    <>
+      <Navbar />
 
-      <h1>404</h1>
+      <section className={styles.notFound}>
+        <h1>404</h1>
 
-      <h2>Page Not Found</h2>
+        <h2>Page Not Found</h2>
 
-      <p>
-        The page you are looking for doesn't exist.
-      </p>
+        <p>
+          The page you are looking for doesn't exist.
+        </p>
 
-      <Link to="/" className={styles.btn}>
-        Go Back Home
-      </Link>
+        <Link to="/" className={styles.btn}>
+          Go Back Home
+        </Link>
+      </section>
 
-    </section>
+      <Footer />
+    </>
   );
 }
 
